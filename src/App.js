4a115bd304f5bed48917be72cdb9ee3ec1d543fe
Jason <jasongin88@gmail.com>
2021-02-18 22:06:09 -0500

    Changed class to functional hooks

--- src/App.js
+++ src/App.js
@@ -14,6 +14,14 @@ export default function App() {
   const [minDate, setMinDate] = useState(new Date("2012-01-01"));
   const [maxDate, setMaxDate] = useState(new Date("2012-01-23"));
 
+  useEffect(function() {
+    handleWebSocket();
+  }, []);
+
+  useEffect(function() {
+    socket.send(JSON.stringify({action:"subscribe", params:"T." + this.state.instrument}));
+  }, [instrument]);
+
   return (
     <div className="app">
       <header><Header /></header>
@@ -64,82 +72,4 @@ function handleWebSocket() {
   socket.onerror = function(error) {
     alert(`Error: ${error.message}`);
   };
-}
-
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      data: sampleData,
-      instrument: "MSFT",
-      minDate: (new Date("2012-01-01")),
-      maxDate: (new Date("2012-01-23"))
-
-    }
-    this.changeInstrument = this.changeInstrument.bind(this);
-  }
-
-  componentDidMount() {
-    this.handleWebSocket();
-  }
-
-  componentDidUpdate() {
-    socket.send(JSON.stringify({action:"subscribe", params:"T." + this.state.instrument}));
-
-  }
-
-  // update data when new pricing data comes in to cause a rerender of chart with updated data
-  unsubscribe() {
-    socket.send(JSON.stringify({action:"unsubscribe", params:"T." + this.state.instrument}));
-    socket.onmessage = (e) => {
-      console.log(e.data);
-    };
-  }
-
-  updateChart() {
-    
-  }
-
-  changeInstrument(cur) {
-    this.unsubscribe();
-    this.setState({
-      instrument: cur
-    })
-    this.updateChart();
-  }
-
-  handleWebSocket() {
-    socket.onopen = (e) => {
-      console.log("Open: Connection established to Polygon.io Stock Streaming WebSocket");
-      socket.send(JSON.stringify(auth));
-      socket.send(JSON.stringify({action:"subscribe", params:"T." + this.state.instrument}));
-      socket.onmessage = (e) => {
-        console.log(`Message: Data received from server: ${e.data}`);
-      };
-    };
-    socket.onclose = function(event) {
-      if (event.wasClean) {
-        alert(`Close: Connection closed successfully, code=${event.code} reason=${event.reason}`);
-      } else {
-        // e.g. server process killed or network down
-        // event.code is usually 1006 in this case
-        alert('Close: Connection died');
-      }
-    };
-    socket.onerror = function(error) {
-      alert(`Error: ${error.message}`);
-    };
-  }
-
-  render() {
-    return (
-    <div className="app">
-      <header><Header /></header>
-      <main>
-        <Instruments defaultInstrument={this.state.instrument} changeHandler={this.changeInstrument} />
-        <Chart data={this.state.data} instrument={this.state.instrument} minDate={this.state.minDate} maxDate={this.state.maxDate} />
-      </main>
-    </div>
-  );
-  }
 }
