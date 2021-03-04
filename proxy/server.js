87e2a3d0e765cc31577ae6171e29b1db01f63261
Jason <jasongin88@gmail.com>
2021-03-03 20:51:30 -0500

    Add server

---
+++ proxy/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const Alpaca = require('@alpacahq/alpaca-trade-api');
+
+const alpaca = new Alpaca({
+    keyId: process.env.API_KEY,
+    secretKey: process.env.SECRET_API_KEY,
+    paper: false,
+    usePolygon: false
+});
+
+const app = express();
+const port = 3001;
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
+
+app.get('/', (req, res) => {
+    alpaca.getBars('1Min', ['AAPL'], {limit: 5}).then((response) => {
+        res.send(JSON.stringify(response));
+    });
+});
+
+app.listen(port, () => console.log('http://localhost:' + port));
